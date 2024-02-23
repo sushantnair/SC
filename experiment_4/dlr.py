974001e2fb52d03b66c4a63278f2bc24a6bc9d69
sushantnair <sushantnair.developer@gmail.com>
2024-02-23 16:33:47 +0530

    Create dlr.py
    Implementation of Delta Learning Rule

---
+++ experiment_4/dlr.py
@@ -0,0 +1,77 @@
+import numpy as np
+i = 1
+def netcalculator(w, d, c, x, l):
+    global i
+    w_transpose = w.T
+    print(f'w{i}_transpose is {w_transpose}')
+    net = np.dot(w_transpose, x)
+    print(f'Net{i} is {net[0][0]:.3f}.')
+    o = 2 / (1 + np.exp(-l * net)) - 1
+    print(f'o{i} is {o[0][0]:.3f}')
+    f_dash_net = 0.5 * (1 - o * o)
+    print(f'f\'(net{i}) is {f_dash_net[0][0]:.3f}')
+    w_return = c * (d - o) * f_dash_net * x + w
+    print(f'w{i+1} is', w_return)
+    i = i + 1
+    return w_return
+
+def main():
+    # Initialize empty lists to store user inputs
+    user_inputs_x1 = []
+    user_inputs_x2 = []
+    user_inputs_x3 = []
+    user_inputs_w1 = []
+    # Extending the capability of the network by making the matrices as n × 1 matrices instead of strictly 4 × 1 matrices
+    n = int(input('Enter the value of n for X1, X2, X3 and W1 n × 1 matrices: '))
+    # Get user input for each element
+    for i in range(n):
+        user_input_x1 = float(input(f"Enter element {i + 1} for x1: "))
+        user_inputs_x1.append(user_input_x1)
+        user_input_x2 = float(input(f"Enter element {i + 1} for x2: "))
+        user_inputs_x2.append(user_input_x2)
+        user_input_x3 = float(input(f"Enter element {i + 1} for x3: "))
+        user_inputs_x3.append(user_input_x3)
+        user_input_w1 = float(input(f"Enter element {i + 1} for w1: "))
+        user_inputs_w1.append(user_input_w1)
+    
+    d1 = int(input('Enter the desired target value 1: '))
+    d2 = int(input('Enter the desired target value 2: '))
+    d3 = int(input('Enter the desired target value 3: '))
+    c = int(input('Enter the learning constant value: '))
+    l = int(input('Enter the lambda value: '))
+
+    # Convert the list of user inputs into a NumPy array
+    x1 = np.array(user_inputs_x1).reshape(n, 1)
+    x2 = np.array(user_inputs_x2).reshape(n, 1)
+    x3 = np.array(user_inputs_x3).reshape(n, 1)
+    w1 = np.array(user_inputs_w1).reshape(n, 1)
+
+    '''# x1 = [1, -2, 0, -1]
+    x1 = np.array([[1], [-2], [0], [-1]])
+    # x2 = [0, 1.5, -0.5, -1]
+    x2 = np.array([[0], [1.5], [-0.5], [-1]])
+    # x3 = [-1, 1, 0.5, -1]
+    x3 = np.array([[-1], [1], [0.5], [-1]])
+    # w1 = [1, -1, 0, 0.5]
+    w1 = np.array([[1], [-1], [0], [0.5]])
+    d1 = -1
+    d2 = -1
+    d3 = 1
+    c = 0.1
+    l = 1'''
+    print(f'X1: {x1}\nX2: {x2}\nX3: {x3}\nW1: {w1}\nc: {c}\nlambda: {l}')
+    w2 = netcalculator(w = w1, d = d1, c = c, x = x1, l = l)
+    w3 = netcalculator(w = w2, d = d2, c = c, x = x2, l = l)
+    w4 = netcalculator(w = w3, d = d3, c = c, x = x3, l = l)
+    print(f'Learning Completed! The final weights are:')
+    i = 0
+    for element in w4:
+        if i == 0 or i == 1:
+            print(f'{element[0]:.3f}', end = ", ")
+        elif i == 2:
+            print(f'{element[0]:.3f}', end = " and ")
+        else:
+            print(f'{element[0]:.3f}')
+        i+=1
+if __name__ == "__main__":
+    main()
